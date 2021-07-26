f2e413aa0a2f175efcbadfdb1b6e95781eb4db97
Ye Thiha <mightycryptowall@gmail.com>
2021-07-27 00:39:37 +0630

    cache with set -> added

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 const express = require("express");
 const redis = require("redis");
-
+const util = require("util");
 const redisURL = "redis://127.0.0.1:6379"
-redis.createClient(redisURL);
+const client = redis.createClient(redisURL);
+
+client.set = util.promisify(client.set);
 
 const app = express();
+app.use(express.json());
 
-
+app.post("/", async (req, res) => {
+    const { key, value } = req.body;
+    const response = await client.set(key, value);
+    res.json(response);
+});
 
 app.listen(8080, () => {
     console.log("Hey, now listening on port 8080!");
