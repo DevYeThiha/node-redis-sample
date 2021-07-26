0ea62d9ac1b21f3aa173c6d34c43908e297adbeb
Ye Thiha <mightycryptowall@gmail.com>
2021-07-27 01:07:54 +0630

    expiration added

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ app.get("/posts/:id", async (req,res) => {
     if (cachedPost) return res.json(JSON.parse(cachedPost));
 
     const response = await axios.get(`https://jsonplaceholder.typicode.com/posts/${id}`);
-    client.set(`post-${id}`, JSON.stringify(response.data));
+    client.set(`post-${id}`, JSON.stringify(response.data), "EX", 10);
     return res.json(response.data);
 });
 
