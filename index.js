3e806abc361cdcb775a14131fe09c90b70e4afb6
Ye Thiha <mightycryptowall@gmail.com>
2021-07-27 00:53:03 +0630

    cache with get -> added

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const { default: axios } = require("axios");
 const express = require("express");
 const redis = require("redis");
 const util = require("util");
@@ -5,6 +6,7 @@ const redisURL = "redis://127.0.0.1:6379"
 const client = redis.createClient(redisURL);
 
 client.set = util.promisify(client.set);
+client.get = util.promisify(client.get);
 
 const app = express();
 app.use(express.json());
@@ -13,6 +15,20 @@ app.post("/", async (req, res) => {
     const { key, value } = req.body;
     const response = await client.set(key, value);
     res.json(response);
+});
+
+app.get("/", async (req, res) => {
+    const { key } = req.body;
+    const value = await client.get(key);
+    res.json(value);
+});
+
+app.get("/posts/:id", async (req,res) => {
+    const { id } = req.params;
+
+    const response = await axios.get(`https://jsonplaceholder.typicode.com/posts/${id}`);
+
+    return res.json(response.data);
 });
 
 app.listen(8080, () => {
